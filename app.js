fa3a6fb4270dd26c95baa80e754136f213755ccf
GitEduardoSS <eduardo_ss@yahoo.com>
2024-09-09 13:11:51 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,12 +5,31 @@ function pesquisar() {
     // Seleciona a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
-    // Inicializa uma string vazia para armazenar os resultados
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    // Se o campo de pesquisa for vazio sai da função
+    if (!campoPesquisa || campoPesquisa == " ") {
+        section.innerHTML = "<p>Nada foi encontrado</p>"
+        return
+    };
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    // console.log(campoPesquisa);
+
+    // Inicializa strings vazias para armazenar os resultados
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre cada dado da pesquisa e constrói o HTML
     for (let dado of dados) {
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // Cria um novo elemeento HTML para cada resultado
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
@@ -19,8 +38,11 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
         `;
+        }
     }
-
+    if (!resultados) {
+        resultados = "<p>Sem resultados para a pesquisa</p>"
+    }
     // Atualiza o conteúdo da seção com os resultados
     section.innerHTML = resultados;
 }
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo: "Rayssa Leal",
         descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
     },
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e força. Com diversas medalhas em competições nacionais e internacionais, ela é um dos principais nomes do judô feminino brasileiro.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" // Substitua por um link real para a Beatriz Souza
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judô ipon judoka"
     },
     {
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta brasileira que encantou o mundo com suas performances na ginástica artística. Com medalhas olímpicas, ela é um símbolo de superação e determinação.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" // Substitua por um link real para a Rebeca Andrade
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ginástica ouro"
     }
 ];
